5bb7987346c641ee392e32f512fcc8bf3d938703
Anders Einar Hilden <hildenae@gmail.com>
2013-09-07 22:41:56 +0200

    Even smaller. More format options.

--- select.py
+++ select.py
@@ -1,67 +1,47 @@
 #!/usr/bin/python
-# Reimplementation of http://digitalfoo.net/posts/drawing-rectangles-on-the-screen-with-xlib-c-library
 import sys,os
-from Xlib import X, display, Xutil, Xatom
+from Xlib import X,display,Xutil,Xatom
 from Xlib.protocol import event
-output = "x(%d,%d), y(%d,%d)" if len(sys.argv) <= 1 else sys.argv[1]
+# Avalible formats are startx, starty, endx, endy, width and height
+output="sx %(startx)d ex %(endx)d sy %(starty)d ey %(endy)d" if len(sys.argv)<=1 else sys.argv[1]
 sys.stdout.flush()
 class NullDevice():
- def write(self, s):
-    pass
-sys.stdout = NullDevice()
-# For some reason, this prints Xlib.protocol.request.QueryExtension
-dis = display.Display() 
-# sys.__stdout__ is provided by python
-sys.stdout = sys.__stdout__
+ def write(self,s):
+  pass
+sys.stdout=NullDevice()
+d=display.Display()
+sys.stdout=sys.__stdout__
 sys.stdout.flush()
-screen = dis.screen()
-window = screen.root
-colormap = screen.default_colormap
-#(r,g,b) blue = (0,0,65535)
-color = colormap.alloc_color(0, 0, 65535)
-# Xor it because we'll draw with X.GXxor function 
-xor_color = color.pixel ^ 0xffffff 
-gc = window.create_gc(
-        line_width = 4,
-        line_style = X.LineSolid,
-        fill_style = X.FillOpaqueStippled,
-        fill_rule  = X.WindingRule,
-        cap_style  = X.CapButt,
-        join_style = X.JoinMiter,
-        foreground = xor_color,
-        background = screen.black_pixel,
-        function = X.GXxor, 
-        graphics_exposures = False,
-        subwindow_mode = X.IncludeInferiors,
-        ) 
-window.grab_pointer ( True, (X.ButtonPressMask | X.ButtonMotionMask | X.ButtonReleaseMask), X.GrabModeAsync, X.GrabModeAsync, X.NONE, X.NONE, X.CurrentTime );
-window.grab_keyboard( True, X.GrabModeAsync, X.GrabModeAsync,X.CurrentTime);
-done = False;
+s=d.screen()
+wi=s.root
+cm=s.default_colormap
+c=cm.alloc_color(0,0,65535)
+xc=c.pixel^0xffffff 
+g=wi.create_gc(line_width=4,line_style=X.LineSolid,fill_style=X.FillOpaqueStippled,fill_rule=X.WindingRule,cap_style=X.CapButt,join_style=X.JoinMiter,foreground=xc,background=s.black_pixel,function=X.GXxor,graphics_exposures=False,subwindow_mode=X.IncludeInferiors,)
+wi.grab_pointer(True,(X.ButtonPressMask|X.ButtonMotionMask|X.ButtonReleaseMask),X.GrabModeAsync,X.GrabModeAsync,X.NONE,X.NONE,X.CurrentTime);
+wi.grab_keyboard(True,X.GrabModeAsync,X.GrabModeAsync,X.CurrentTime);
+done=False;
 while not done:
-    event = dis.next_event()
-    if event.type == X.ButtonPress:
-        startx = event.root_x;
-        starty = event.root_y;
-        rcx = rcy = rcw = rch = 0;
-        window.rectangle( gc, rcx, rcy, rcw, rch);
-        rcx = event.root_x;
-        rcy = event.root_y;
-    elif event.type == X.ButtonRelease:
-        endx = event.root_x;
-        endy = event.root_y;
-        done = True;
-    elif event.type == X.MotionNotify:
-        window.rectangle( gc, rcx, rcy, rcw, rch);
-        rcw = abs(rcx - event.root_x);
-        rch = abs(rcy - event.root_y);
-        if (rcx > event.root_x):
-            rcx = event.root_x;
-        if (rcy > event.root_y):
-            rcy = event.root_y;
-        window.rectangle( gc, rcx, rcy, rcw, rch);
-dis.ungrab_pointer ( X.CurrentTime );
-dis.ungrab_keyboard ( X.CurrentTime );
-window.rectangle( gc, rcx, rcy, rcw, rch);
-dis.flush();
-dis.close();
-print output % (rcx, rcy, rcw, rch); 
+ e=d.next_event()
+ if e.type==X.ButtonPress:
+  x=y=w=h=0;
+  wi.rectangle(g,x,y,w,h);
+  x=e.root_x;
+  y=e.root_y;
+ elif e.type==X.ButtonRelease:
+  done=True;
+ elif e.type==X.MotionNotify:
+  wi.rectangle(g,x,y,w,h);
+  w=abs(x-e.root_x);
+  h=abs(y-e.root_y);
+  if(x>e.root_x):
+   x=e.root_x;
+  if(y>e.root_y):
+   y=e.root_y;
+  wi.rectangle(g,x,y,w,h);
+d.ungrab_pointer(X.CurrentTime);
+d.ungrab_keyboard(X.CurrentTime);
+wi.rectangle(g,x,y,w,h);
+d.flush();
+d.close();
+print output % {'startx':x,'starty':y,'endx':x+w,'endy':y+h,'width':w,'height':h}
